5e1b15f9322eade86deffa678cf1417c7571b144
Jorge García <79732412+JHUMBERTTO@users.noreply.github.com>
2022-04-04 17:45:23 -0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,8 @@
+print("welcome to the band name generator.")
+
+city_name = input("whats the name of the city you grew up?")
+
+pets_name = input("whats your name pets?")
+
+print("Your band name could be:" + city_name + "" + pets_name)
+
